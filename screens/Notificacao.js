8b45e7df2af109aa11509b26853448dd68e39451
jackson oliveira <meg.ltf@gmail.com>
2020-10-27 18:18:05 -0300

    atulizado

---
+++ screens/Notificacao.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text} from 'react-native';
+
+// import { Container } from './styles';
+
+function Notifications() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Notifications Screen</Text>
+      </View>
+    );
+  }
+  export default Notifications
